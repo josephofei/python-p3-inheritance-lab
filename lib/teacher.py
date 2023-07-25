00682e1c99e884af16d7902e16133d19fca6a096
Joseph Ofei <josephofei24@gmail.com>
2023-07-25 10:31:33 -0400

    object inheritant lab complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,33 @@ from user import User
 import random
 
 class Teacher(User):
+    
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+            knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+            ]
+            self.knowledge = knowledge
+            value = random.randint(0,7)
+            return knowledge[value]
+        
+        
